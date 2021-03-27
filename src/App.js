4ccb085207867ee9c8ef8c32ddc26f01a190fea1
Templars <antoine.ratat@gmail.com>
2021-03-27 19:55:30 +0800

    update screenshots

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import './App.css'
 import ImageList from './components/ImageList'
 
-// References
-// https://medium.com/javascript-in-plain-english/lazy-load-images-with-the-intersection-observer-api-and-react-4fd8ae19e195
-// https://www.taraojo.com/post/lazy-loading-images
-// https://codesandbox.io/s/intersectionobserverreact-zek38?from-embed=&file=/src/App.js
-// https://medium.com/walmartglobaltech/lazy-loading-images-intersectionobserver-8c5bff73092
-// https://www.smashingmagazine.com/2020/03/infinite-scroll-lazy-image-loading-react/
-
 function App() {
 	return (
 		<div className='App'>
